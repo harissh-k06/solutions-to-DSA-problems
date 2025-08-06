02caebb67c53ba1031be701133ef174aeefe1638
harissh-k06 <harisshkrishna@gmail.com>
2025-08-07 02:35:11 +0530

    1 ms (34.16%) 9 MB (61.71%) Easy .cpp

--- 0067-add-binary/0067-add-binary.cpp
+++ 0067-add-binary/0067-add-binary.cpp
@@ -1,66 +1,56 @@
-
 class Solution {
-public:
-    string addBinary(string a, string b) {
-        int aLen = a.length(), bLen = b.length();
-        string answer;
-        char c = '0'; // carry as char
-        int indexA = aLen - 1, indexB = bLen - 1;
-        bool flag = false;
-
-        // Main loop
-        while(indexA >= 0 && indexB >= 0){
-            char bita = a[indexA];
-            char bitb = b[indexB];
-            char xor1 = xOR(c, bita);
-            char result = xOR(xor1, bitb);
-            answer += result;
-
-            // Update the carry (flag)
-            flag = carryExists(c, bita, bitb);
-            c = (flag ? '1' : '0');
-            indexA--;
-            indexB--;
-        }
-
-        // Leftover in a
-        while (indexA >= 0){
-            char bita = a[indexA];
-            char result = xOR(c, bita);
-            answer += result;
-
-            flag = carryExists(c, bita, '0');
-            c = (flag ? '1' : '0');
-            indexA--;
-        } 
-
-        // Leftover in b
-        while (indexB >= 0){
-            char bitb = b[indexB];
-            char result = xOR(c, bitb);
-            answer += result;
-
-            flag = carryExists(c, bitb, '0');
-            c = (flag ? '1' : '0');
-            indexB--;
-        } 
-
-        // Final carry
-        if (c == '1') answer += '1';
-
-        reverse(answer.begin(), answer.end());
-        return answer;
-    }
 
 private:
-    // XOR for two chars
-    char xOR(char a, char b){
-        return (a == b) ? '0' : '1';
+    char xOR(char a  , char b){
+        return a==b ? '0' : '1';
     }
 
-    // Carry for THREE bits: true if at least two of the bits are '1'
-    bool carryExists(char a, char b, char d){
-        int cnt = (a == '1') + (b == '1') + (d == '1');
-        return cnt >= 2;
+    char carryGenerator(char a , char b , char c){
+        int noOf1 = 0;
+        if (a == '1') noOf1++;
+        if (b == '1') noOf1++;
+        if (c == '1') noOf1++;
+        return (noOf1 >=2) ? '1' : '0';
+    }
+
+public:
+    string addBinary(string a, string b) {
+        int lena = a.length() , lenb = b.length();
+        int i = lena - 1 , j = lenb - 1;
+
+        string ans = "";
+        int carry = '0';
+        while (i >= 0 && j>=0){
+            // carry + a + b
+            char no1= a[i] , no2 = b[j];
+            char binNo = xOR(carry , no1);  //  0 1 1 or 1 1 1 or 1 0 1
+            binNo = xOR(binNo , no2); 
+            ans+=binNo;
+            carry = carryGenerator(carry , no1, no2);
+            i-- ; j--;
+        }
+
+        while (i>=0){
+            char no1 = a[i];
+            char binNo = xOR(carry , no1);
+            ans+=binNo;
+            carry = carryGenerator(carry , no1 , '0');
+            i--;
+        }
+
+        while (j>=0){
+            char no2 = b[j];
+            char binNo = xOR(carry , no2);
+            ans+=binNo;
+            carry = carryGenerator(carry , no2 , '0');
+            j--;
+        }
+
+        if (carry == '1'){
+            ans+=carry;
+        }
+
+        reverse(ans.begin() , ans.end());
+        return ans;
     }
 };
