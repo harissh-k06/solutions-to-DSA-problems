62146e527fadfa551da6b4440f83324d0bf177ac
harissh-k06 <harisshkrishna@gmail.com>
2025-08-07 02:11:04 +0530

    7 ms (41.86%) 23.7 MB (94.2%) Medium .cpp

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,15 +1,23 @@
-
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        //sort(intervals.begin(), intervals.end());
+        if (intervals.empty()) { // edge case
+            return {};
+        }
+
+        sort(intervals.begin(), intervals.end()); // correct
+
         vector<vector<int>> merged;
-        merged.push_back(intervals[0]);
+        merged.push_back(intervals[0]); 
+
         for (int i = 1; i < intervals.size(); i++) {
-            if (merged.back()[1] >= intervals[i][0]) {
-                merged.back()[1] = max(merged.back()[1], intervals[i][1]);
+            vector<int>& lastMerged = merged.back(); 
+            vector<int>& current = intervals[i]; 
+
+            if (lastMerged[1] >= current[0]) { 
+                lastMerged[1] = max(lastMerged[1], current[1]); 
             } else {
-                merged.push_back(intervals[i]);
+                merged.push_back(current); 
             }
         }
         return merged;
