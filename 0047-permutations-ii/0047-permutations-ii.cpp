a68c8ec681b7f02af8d84887888592086a67b965
Harissh Krishna L <harisshkrishna@gmail.com>
2025-08-09 23:35:24 +0530

    3 ms (66.9%) 12 MB (60.66%) Medium .cpp

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -1,31 +1,41 @@
 class Solution {
 
 private:
-   set<vector<int>> dfs(vector<int> nums){
-        if (nums.size() ==0) return {{}};
-        vector<int> temp(nums.begin()+1 , nums.end());
-        set<vector<int>> perms = dfs(temp);
-        set<vector<int>> res;
-        for (const auto& perm: perms){
-            for (int i = 0 ; i<=perm.size() ; i++){
-                vector<int> p_copy = perm;
-                p_copy.insert(p_copy.begin() + i , nums[0]);
-                res.insert(p_copy);
+    vector<vector<int>> res;
+    unordered_map<int , int> freqMap;
+
+    void populateFreqMap(vector<int>& nums){
+        for (int num : nums){
+            freqMap[num]++;
+        }
+    }
+
+    void dfs(vector<int>& nums , vector<int>& perm){
+        if (nums.size() == perm.size()){
+            res.emplace_back(perm);
+            return;
+        }
+
+        for (const auto& [num , freq] : freqMap){
+            if (freq>0){
+                perm.emplace_back(num);
+                freqMap[num]--;
+                dfs(nums , perm);
+                freqMap[num]++;
+                perm.pop_back();
             }
         }
 
-        return res;
-   }
 
+    }
 
 public:
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-        //
-        set<vector<int>> res = dfs(nums );
-        vector<vector<int>> ans;
-        for (const auto& perm : res){
-            ans.emplace_back(perm);
-        }
-        return ans;
+        int size = nums.size();
+        if (size == 0 ) return {{}};
+        populateFreqMap(nums);
+        vector<int> perm;
+        dfs(nums , perm);
+        return res;
     }
 };
