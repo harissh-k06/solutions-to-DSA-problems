6e799d7def6b636833b4d093436d8c3845d3dd7b
Harissh Krishna L <harisshkrishna@gmail.com>
2025-08-09 21:34:37 +0530

    0 ms (100%) 9.1 MB (89.03%) Easy .cpp

--- 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.cpp
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.cpp
@@ -2,24 +2,19 @@ class Solution {
 public:
     int subsetXORSum(vector<int>& nums) {
         int sumXOR = 0;
-        vector<int> subset ; 
-        dfs(nums , 0 , subset , sumXOR);
+         
+        dfs(nums , 0 , 0 , sumXOR);
         return sumXOR;
     }
 
 
-    void dfs(vector<int>& nums , int idx , vector<int>& subset ,  int& sumXOR){
+    void dfs(vector<int>& nums , int idx , int currXOR ,  int& sumXOR){
         if (idx == nums.size()){
-            int xORSum = 0;
-            for (int num : subset){
-                xORSum = xORSum ^ num;
-            }
-            sumXOR = sumXOR + xORSum;
+            sumXOR +=currXOR;
             return;
         }
-        subset.emplace_back(nums[idx]);
-        dfs(nums , idx+1 , subset , sumXOR);
-        subset.pop_back();
-        dfs(nums , idx+1 , subset , sumXOR);
+        dfs(nums , idx+1 , currXOR ^ nums[idx] , sumXOR);
+        
+        dfs(nums , idx+1 , currXOR , sumXOR);
     }
 };
