476d3962574e91b32f8f4e4f3546c3b47f7f6fa7
Harissh Krishna L <harisshkrishna@gmail.com>
2025-08-10 01:02:53 +0530

    Time: 15 ms (13.47%), Space: 14.4 MB (15.47%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,41 +1,50 @@
 class Solution {
+private:
+    unordered_set<int> colTrack;
+    unordered_set<int> positiveDiagTrack;
+    unordered_set<int> negativeDiagTrack;
+    vector<vector<string>> res;
 public:
     vector<vector<string>> solveNQueens(int n) {
-        vector<vector<string>> board(n , vector<string>(n,"."));
-        unordered_set<int> cols;
-        unordered_set<int> positiveDiag;
-        unordered_set<int> negativeDiag;
-        vector<vector<string>> res;
-        backtrack(board , n , 0 ,cols , positiveDiag , negativeDiag , res);
+        vector<vector<char>> board(n, vector<char>(n, '.'));
+        nQueensSolver(board , 0 , n);
         return res;
+
     }
 
 private:
-    void backtrack(vector<vector<string>>& board , const int& n , int row ,unordered_set<int>& cols,unordered_set<int>& positiveDiag,unordered_set<int>& negativeDiag , vector<vector<string>>& res){
+    void nQueensSolver(vector<vector<char>>& board, int row, const int& n) {
         if (row == n){
             vector<string> solution;
-            for (int i = 0 ; i < n; i++){
-                string row = "";
-                for (int j = 0 ; j<n; j++){
-                    row+=board[i][j];
+            for (int r = 0 ; r < n ; r++){
+                string rowSol = "";
+                for (int c = 0 ; c < n ; c++){
+                    rowSol+=board[r][c];
                 }
-                solution.push_back(row);
+                solution.emplace_back(rowSol);
             }
-            res.push_back(solution);
+            res.emplace_back(solution);
         }
 
-        for (int c = 0 ; c < n ; c++){
-            if (cols.find(c)!=cols.end() || positiveDiag.find(row+c)!=positiveDiag.end() || negativeDiag.find(row-c)!=negativeDiag.end() ) continue;
+
+
+        for (int col = 0; col < n; col++) {
+            bool flag =
+                (colTrack.find(col) != colTrack.end()) ||
+                (positiveDiagTrack.find(row + col) != positiveDiagTrack.end()) ||
+                (negativeDiagTrack.find(row - col) != negativeDiagTrack.end());
+
+            if (flag) continue;
             else{
-                board[row][c] = "Q";
-                cols.insert(c);
-                positiveDiag.insert(row+c);
-                negativeDiag.insert(row-c);
-                backtrack(board , n , row+1  , cols , positiveDiag , negativeDiag , res);
-                board[row][c] = ".";
-                cols.erase(c);
-                positiveDiag.erase(row+c);
-                negativeDiag.erase(row-c);
+                board[row][col] = 'Q';
+                colTrack.insert(col);
+                negativeDiagTrack.insert(row - col);
+                positiveDiagTrack.insert(row + col);
+                nQueensSolver(board , row+1 , n);
+                colTrack.erase(col);
+                negativeDiagTrack.erase(row - col);
+                positiveDiagTrack.erase(row + col);
+                board[row][col] = '.';
             }
         }
     }
