11f7f78b87665017924fe4954d82aa93f339c700
Harissh Krishna L <harisshkrishna@gmail.com>
2025-08-21 19:37:37 +0530

    Time: 0 ms (100%), Space: 8.9 MB (6.62%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -0,0 +1,35 @@
+class Solution {
+private:
+    string toBin(int n){
+        string bin = "";
+        while (n!= 1 && n!=0){
+            if (n % 2 == 0) bin+='0';
+            else bin+='1';
+            n/=2;
+        }
+        if (n == 1) bin+='1';
+        while (bin.length() < 32){
+            bin+='0';
+        }
+        reverse(bin.begin() , bin.end());
+        return bin;
+    }
+
+    int toNum(string bin){
+        int power = bin.length() -1;
+        int num = 0;
+        for (char c : bin){
+            int mul = c == '1' ? 1 : 0;
+            num+=pow(2,power--)*mul;
+        }
+        return num;
+    }
+
+public:
+    int reverseBits(int n) {
+        string bin = toBin(n);
+        cout<<toBin(12);
+        reverse(bin.begin()  , bin.end());
+        return toNum(bin);
+    }
+};
