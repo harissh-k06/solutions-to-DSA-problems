4dfdb586015686136601ff3d356cb90f0679874f
Harissh Krishna L <harisshkrishna@gmail.com>
2025-08-21 23:10:35 +0530

    Time: 0 ms (100%), Space: 16.5 MB (5.31%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.cpp
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.cpp
@@ -11,21 +11,20 @@
 class Solution {
 
 private:
-    vector<ListNode*> uniq;
-
+    vector<ListNode*> uniq; //space O(n)
 public:
     ListNode* deleteDuplicates(ListNode* head) {
         if (!head) return head;
         ListNode* curr = head->next;
         ListNode* prev = head;
         uniq.emplace_back(prev);
-        while (curr){
+        while (curr){ //time O(n)
             if (curr->val != prev->val) uniq.emplace_back(curr);
             prev = curr;
             curr = curr->next;
         }
         curr = head;
-        for ( auto& node : uniq){
+        for ( auto& node : uniq){ //O(n)
             if (node == head) continue;
             curr->next = node;
             curr= curr->next;
