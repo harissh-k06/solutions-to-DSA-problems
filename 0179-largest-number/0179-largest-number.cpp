bc1179a6e3bea75e530bda7f9e2eb820c66d91b1
Harissh Krishna L <harisshkrishna@gmail.com>
2025-08-25 01:12:33 +0530

    Time: 3 ms (76.39%), Space: 17.3 MB (6.02%) - LeetHub

---
+++ 0179-largest-number/0179-largest-number.cpp
@@ -0,0 +1,36 @@
+class Compare{
+    public:
+
+    bool operator() (const string& a , const string& b){
+        if( (a + b) < (b + a)) return true;
+        else return false;
+    }
+};
+
+
+class Solution {
+
+private:
+    priority_queue<string , vector<string> , Compare> pq;
+
+public:
+    string largestNumber(vector<int>& nums) {
+        vector<string> toS;
+        
+        bool isAllZero = true;
+        for (int num : nums){
+            toS.push_back(to_string(num));
+            if (!(isAllZero && num == 0)) isAllZero = false; 
+        }
+        if (isAllZero) return "0";
+        for (string num : toS){
+            pq.push(num);
+        }
+        string res = "";
+        while (!pq.empty()){
+            res+=pq.top();
+            pq.pop();
+        }
+        return res;
+    }
+};
