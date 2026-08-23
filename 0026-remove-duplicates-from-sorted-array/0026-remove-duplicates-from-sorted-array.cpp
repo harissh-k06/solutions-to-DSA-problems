67d8a85b42915d77bee47bcaaf2100beca6dcb4f
Harissh Krishna L <127003093@sastra.ac.in>
2026-08-23 16:59:47 +0530

    Time: 0 ms (100%), Space: 22.5 MB (98.11%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -3,9 +3,11 @@ public:
     int removeDuplicates(vector<int>& nums) {
         int left = 1 , right = 1;
         while (right < nums.size()){
-            if (nums[right]!=nums[right-1]){
-                nums[left++] = nums[right];
-            }
+            
+                if (nums[right]!=nums[right-1]){
+                    nums[left++] = nums[right];
+                }
+            
             right++;
         }
         return left;
