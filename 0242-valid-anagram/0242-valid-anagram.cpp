fc3ae5fa74b6ff197c50fb8a71a55d94ce455b42
Harissh Krishna L <127003093@sastra.ac.in>
2026-09-03 18:16:34 +0530

    Time: 2 ms (49.85%), Space: 10 MB (11.47%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if (s.length()!=t.length()) return false;
-        unordered_map<char , int> freq1;
-        unordered_map<char , int> freq2;
+        unordered_map<char , int> freqS;
+        unordered_map<char , int> freqT;
         for (char c : s){
-            freq1[c]+=1;
+            freqS[c]++;
         }
-        for (char c : t){
-            freq2[c]+=1;
+        for (char c: t){
+            freqT[c]++;
         }
 
-        for (char c : s){
-            if (freq1[c]!=freq2[c]) return false;
+        for (const auto [k,v] : freqS){
+            if (freqT.contains(k) && freqT[k] == v) continue;
+            else return false;
         }
-        return true;
+        return s.length() == t.length();
     }
 };
