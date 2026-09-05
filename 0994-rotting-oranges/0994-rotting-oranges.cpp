012dfe3b10306e9632418d1222d444f9284b5309
Harissh Krishna L <127003093@sastra.ac.in>
2026-09-06 00:50:22 +0530

    Time: 0 ms (100%), Space: 16.7 MB (69.11%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,71 +1,65 @@
 class Solution {
+
 private:
-   
-    bool checkFresh(vector<vector<int>>& grid, int i, int j) {
-        int m = grid.size();
-        int n = grid[0].size();
-        if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] != 1) return false;
+    bool checkFresh(vector<vector<int>>& grid , int i , int j){
+        if (i < 0 || j >= grid[0].size() || i>=grid.size() || j < 0 || grid[i][j]!=1) return false;
         return true;
     }
 
+
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
-        queue<pair<int, int>> bfsQ;
+        int n = grid.size();
+        int m = grid[0].size();
+        queue<pair<int , int>> rotten;
         int fresh = 0;
+        for (int i = 0 ; i < grid.size() ; i++){
+            for (int j = 0 ; j < grid[0].size() ; j++) {
+                if (grid[i][j] == 1) fresh++;
+                if (grid[i][j] == 2) rotten.push({i , j});
+            }
+        }
+        
         int time = 0;
-
-     
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 2) {
-                    bfsQ.push({i, j});
-                } else if (grid[i][j] == 1) {
-                    fresh++;
+        while (!rotten.empty()){
+            int currentRotten = rotten.size();
+            bool didWeRot = false;
+            for (int process = 0 ; process < currentRotten ; process++){
+                auto badOrange = rotten.front();
+                rotten.pop();
+                int i = badOrange.first; int j = badOrange.second;
+                if (checkFresh(grid , i + 1 , j)){
+                    fresh--;
+                    grid[i+1][j] = 2;
+                    rotten.push({i+1 , j});
+                    didWeRot = true;
                 }
+
+                if (checkFresh(grid , i - 1 , j)){
+                    fresh--;
+                    grid[i-1][j] = 2;
+                    rotten.push({i-1 , j});
+                    didWeRot = true;
+                } 
+
+                if (checkFresh(grid , i , j+1)){
+                    fresh--;
+                    grid[i][j+1] = 2;
+                    rotten.push({i , j+1});
+                    didWeRot = true;
+                } 
+
+
+                if (checkFresh(grid , i , j-1)){
+                    fresh--;
+                    grid[i][j-1] = 2;
+                    rotten.push({i , j-1});
+                    didWeRot = true;
+                }
+                
             }
+            if (didWeRot) time++;
         }
-
-        if (fresh == 0) return 0;
-
-        while (!bfsQ.empty()) {
-            int size = bfsQ.size();
-            bool changed = false; 
-            for (int k = 0; k < size; k++) {
-                pair<int, int> rottedPair = bfsQ.front();
-                bfsQ.pop();
-                int r = rottedPair.first;
-                int c = rottedPair.second;
-
-                if (checkFresh(grid, r + 1, c)) {
-                    grid[r + 1][c] = 2; 
-                    bfsQ.push({r + 1, c});
-                    fresh--;
-                    changed = true;
-                }
-                if (checkFresh(grid, r - 1, c)) {
-                    grid[r - 1][c] = 2;
-                    bfsQ.push({r - 1, c});
-                    fresh--;
-                    changed = true;
-                }
-                if (checkFresh(grid, r, c + 1)) {
-                    grid[r][c + 1] = 2;
-                    bfsQ.push({r, c + 1});
-                    fresh--;
-                    changed = true;
-                }
-                if (checkFresh(grid, r, c - 1)) {
-                    grid[r][c - 1] = 2; 
-                    bfsQ.push({r, c - 1});
-                    fresh--;
-                    changed = true;
-                }
-            }
-            if (changed) time++; 
-        }
-
         return fresh == 0 ? time : -1;
     }
 };
