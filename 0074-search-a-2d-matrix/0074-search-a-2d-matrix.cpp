1a3ce5ded114ec4a8a99d7256cabd4c6060cabea
Harissh Krishna L <127003093@sastra.ac.in>
2026-09-07 00:06:34 +0530

    Time: 0 ms (100%), Space: 13.5 MB (6.52%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,33 +1,33 @@
 class Solution {
 
 private:
-
-    bool binarySearch(vector<int>& arr ,const int target){
-        int low =  0 , high = arr.size() -1;
-        while (low  <= high){
-            int mid = (low + high) / 2;
-            if (arr[mid] == target ) return true;
-            else if (arr[mid] > target){
-                high = mid - 1;
-            }
-            else{
-                low = mid+1;
-            }
-
+    int findRow(vector<vector<int>>& grid , int target){
+        int low = 0 , high = grid.size()-1;
+        while (low <= high){
+            int mid = (high - low)/2 + low;
+            if (target>=grid[mid][0] && target<=grid[mid][grid[0].size()-1]) return mid;
+            else if (target < grid[mid][0]) high = mid - 1;
+            else if (grid[mid][grid[0].size()-1]<target) low = mid+1;
         }
-        return false;
+        return -1;
     }
 
-
+    int findTarget(vector<int>& arr, int target){
+        int low = 0 , high = arr.size()-1;
+        while (low<=high){
+            int mid = (high - low) / 2 + low;
+            if (arr[mid]==target) return mid;
+            else if (arr[mid] < target) low = mid+1;
+            else high = mid-1;
+        }
+        return -1;
+    }
 
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int rows = matrix.size() ; 
-        int cols = matrix[0].size();
-        for (int i = 0 ; i < rows ; i++){
-            if (target >= matrix[i][0] && target<=matrix[i][cols - 1])
-                return binarySearch(matrix[i] , target);
-        } //O(rows * log(cols))
-        return false;
+        int r = findRow(matrix , target);
+        if (r==-1) return false;
+        return !(findTarget(matrix[r] , target)==-1);
+
     }
 };
