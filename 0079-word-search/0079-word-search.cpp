e10d6ae4366e8761a98579e85ed74fcc4f45584e
Harissh Krishna L <127003093@sastra.ac.in>
2026-09-08 02:08:39 +0530

    Time: 0 ms (100%), Space: 10.6 MB (55.48%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,28 +1,58 @@
 class Solution {
+
+private:   
+    bool dfs(vector<vector<char>>& board, int idx, int i, int j, const string& word){
+        int m = board.size();
+        int n = board[0].size();
+        // Check bounds and character match (visited cells marked as '#' will naturally mismatch)
+        if (i < 0 || i >= m || j < 0 || j >= n || board[i][j] != word[idx]) return false;
+        // Base case: matched full word up to the last character
+        if (idx == word.length() - 1) return true;
+
+        // 1. Mark in-place to avoid std::set allocations
+        char temp = board[i][j];
+        board[i][j] = '#';
+
+        // 2. Short-circuit exploration across 4 directions
+        bool flag = dfs(board, idx + 1, i + 1, j, word) ||
+                    dfs(board, idx + 1, i - 1, j, word) ||
+                    dfs(board, idx + 1, i, j + 1, word) ||
+                    dfs(board, idx + 1, i, j - 1, word);
+            
+        // 3. Backtrack: restore original character
+        board[i][j] = temp;
+
+        return flag;
+    }
+
 public:
     bool exist(vector<vector<char>>& board, string word) {
         int m = board.size();
         int n = board[0].size();
-        char startChar = word[0];
-        for (int i = 0 ; i < m ; i++){
-            for (int j = 0 ; j < n ; j ++){
-                if (startChar == board[i][j]){
-                   if (dfs(board , word , i , j , 0)) return true; 
+
+        // Optimization 1: Frequency check & board capacity
+        if (word.length() > m * n) return false;
+        unordered_map<char, int> boardFreq, wordFreq;
+        for (const auto& row : board) {
+            for (char c : row) boardFreq[c]++;
+        }
+        for (char c : word) {
+            if (++wordFreq[c] > boardFreq[c]) return false;
+        }
+
+        // Optimization 2: Reverse word if the tail character is rarer than the head
+        if (boardFreq[word.back()] < boardFreq[word.front()]) {
+            reverse(word.begin(), word.end());
+        }
+
+        // Search the board
+        for (int i = 0; i < m; i++){
+            for (int j = 0; j < n; j++){
+                if (word[0] == board[i][j]){
+                    if (dfs(board, 0, i, j, word)) return true;
                 }
             }
         }
         return false;
     }
-
-private:
-    bool dfs(vector<vector<char>>& board, const string word , int i , int j , int idx){
-        if (idx == word.length()) return true;    
-        if (i< 0 || i>=board.size() || j< 0 || j>=board[0].size() || board[i][j] != word[idx] || board[i][j] == '\0') return false;
-        char temp = board[i][j];
-        board[i][j] = '\0';
-        bool flag = dfs(board , word , i+1 , j , idx+1) || dfs(board , word , i-1 , j , idx+1) || dfs(board , word , i , j+1 , idx+1 ) || dfs(board , word , i , j-1 , idx+1);
-        board[i][j] = temp;
-        return flag;
-    }
-
 };
