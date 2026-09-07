24fb68b3c305d7a06059b7c1b36e470edd662971
Harissh Krishna L <127003093@sastra.ac.in>
2026-09-08 01:15:56 +0530

    Time: 3 ms (87.98%), Space: 10.1 MB (6.46%) - LeetHub

---
+++ 1095-find-in-mountain-array/1095-find-in-mountain-array.cpp
@@ -0,0 +1,70 @@
+/**
+ * // This is the MountainArray's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * class MountainArray {
+ *   public:
+ *     int get(int index);
+ *     int length();
+ * };
+ */
+
+class Solution {
+
+private:
+    int findMax(int target, MountainArray &mountainArr){
+        int low = 0 , high = mountainArr.length() - 1;
+        while (low <= high){
+            int mid = ( high - low) / 2 + low;
+            int midM = mountainArr.get(mid);
+            int midL  = mid - 1 >= 0 ? mountainArr.get(mid - 1) : -1;
+            int midH = mid + 1 <= mountainArr.length() - 1 ? mountainArr.get(mid + 1) : -1;
+            if ( midM > midL && midM > midH) return mid;
+            else if (midM > midL && midM < midH) low = mid+1;
+            else if (midM < midL && midM > midH) high = mid-1;
+        }
+        return -1;
+    }
+
+    int binarySearch1(int target, MountainArray &mountainArr, int high){
+        int low = 0;
+        while (low <= high){
+            int mid = (high - low) / 2  + low;
+            int midV = mountainArr.get(mid);
+            if ( midV == target) return mid;
+            else if (midV < target){
+                low = mid + 1 ;
+            }
+            else{
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    int binarySearch2(int target, MountainArray &mountainArr, int low){
+        int high = mountainArr.length() - 1 ;
+        while (low <= high){
+            int mid = (high - low) / 2  + low;
+            int midV = mountainArr.get(mid);
+            if ( midV == target) return mid;
+            else if (midV > target){
+                low = mid + 1 ;
+            }
+            else{
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+
+public:
+    int findInMountainArray(int target, MountainArray &mountainArr) {
+        int maxIdx = findMax(target  ,mountainArr);
+        int potIdx = binarySearch1(target , mountainArr ,  maxIdx);
+        if (potIdx == -1){
+            potIdx = binarySearch2(target , mountainArr ,  maxIdx);
+        }
+        return potIdx;
+    }
+};
