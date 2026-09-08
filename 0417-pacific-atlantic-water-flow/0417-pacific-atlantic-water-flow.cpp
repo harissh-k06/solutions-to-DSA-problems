b01303ba4b24a77e73b08a30aa5894015cb5c5c1
Harissh Krishna L <127003093@sastra.ac.in>
2026-09-08 19:08:54 +0530

    Time: 2 ms (89.62%), Space: 22.1 MB (95.28%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
@@ -7,18 +7,17 @@ private:
         int m = heights.size();
         int n = heights[0].size();
         
-        // Out of bounds OR water cannot flow uphill/stay level from ocean's perspective
+  
         if (i < 0 || i >= m || j < 0 || j >= n || heights[i][j] < prevHeight) return;
         
-        // Skip if already visited
+    
         if (isPacific && pac[i][j]) return;
         if (!isPacific && ata[i][j]) return;
 
-        // Mark as reachable
+      
         if (isPacific) pac[i][j] = true;
         else ata[i][j] = true;
 
-        // Explore 4-directional neighbors
         dfs(i + 1, j, heights[i][j], heights, isPacific);
         dfs(i - 1, j, heights[i][j], heights, isPacific);
         dfs(i, j + 1, heights[i][j], heights, isPacific);
@@ -30,22 +29,21 @@ public:
         int m = heights.size();
         int n = heights[0].size();
 
-        // Fix 1: Properly initialize boolean matrices
         pac.assign(m, vector<bool>(n, false));
         ata.assign(m, vector<bool>(n, false));
 
-        // Fix 2: Launch DFS from Pacific (top/left) and Atlantic (bottom/right) borders
+        
         for (int i = 0; i < m; i++) {
-            dfs(i, 0, heights[i][0], heights, true);       // Pacific Left Border
-            dfs(i, n - 1, heights[i][n - 1], heights, false); // Atlantic Right Border
+            dfs(i, 0, heights[i][0], heights, true);      
+            dfs(i, n - 1, heights[i][n - 1], heights, false); 
         }
 
         for (int j = 0; j < n; j++) {
-            dfs(0, j, heights[0][j], heights, true);       // Pacific Top Border
-            dfs(m - 1, j, heights[m - 1][j], heights, false); // Atlantic Bottom Border
+            dfs(0, j, heights[0][j], heights, true);     
+            dfs(m - 1, j, heights[m - 1][j], heights, false); 
         }
 
-        // Collect all coordinates that can reach both oceans
+
         vector<vector<int>> result;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
