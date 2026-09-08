92b477f33870c4cefb3e8e8774b460c64348fcf9
Harissh Krishna L <127003093@sastra.ac.in>
2026-09-08 10:31:54 +0530

    Time: 0 ms (100%), Space: 9.3 MB (41.1%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.cpp
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -3,21 +3,19 @@ public:
     int longestPalindrome(string s) {
         unordered_map<char , int> freq;
         for (char c :  s) freq[c]+=1;
-        int maxLen = 0;
+        int maxLen = 0; bool flag = false;
         for (auto& [k,v] : freq){
             while (v > 1){
                 freq[k]-=2;
                 maxLen+=2;
             }
-        }
-
-        for (auto& [k,v] : freq){
-            if (v == 1){
-                freq[k]-=1;
+            if( v==1 && !flag){
                 maxLen+=1;
-                break;
+                freq[k]-=1;
+                flag= true;
             }
         }
+
         return maxLen;
 
     }
